6469d53ce31a11a932241a94a46efd1b8b6e7249
Eya Sammari <73797676+eyasam@users.noreply.github.com>
2025-01-22 17:52:38 +0100

    Update FuncBegin.java

--- src/java/compiler/TAC/FuncBegin.java
+++ src/java/compiler/TAC/FuncBegin.java
@@ -1,23 +1,22 @@
-package C3A;
+package TAC;
 
-public class FuncBegin extends Instruction {
-  private Symbol funcSymb;
+public class FuncBegin implements NoeudAST {
+    private String functionName;
 
-  public FuncBegin(Symbol funcName) {
-    this.funcSymb = funcName;
-  }
+    public FuncBegin(String functionName) {
+        this.functionName = functionName;
+    }
 
-  @Override
-  public String toString() {
-    return "func begin " + funcSymb.getValue();
-  }
+    public String getFunctionName() {
+        return functionName;
+    }
 
-  @Override
-  public String toPython(Indent indent) {
-    String s = indent + "def " + funcSymb.getValue() + "():\n";
-    indent.inc();
-    s += indent + "global stack\n";
-    s += indent + "output = []";
-    return s;
-  }
+    public void setFunctionName(String functionName) {
+        this.functionName = functionName;
+    }
+
+    @Override
+    public String toString() {
+        return "function : " + functionName ;
+    }
 }
