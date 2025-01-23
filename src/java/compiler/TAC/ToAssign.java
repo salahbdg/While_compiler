2e3924d0dbdba1d537b38cfb7a4bae01b8cd6e02
Eya Sammari <73797676+eyasam@users.noreply.github.com>
2025-01-23 01:53:09 +0100

    Delete src/java/compiler/TAC/ToAssign.java

--- src/java/compiler/TAC/ToAssign.java
+++
@@ -1,9 +0,0 @@
-package C3A;
-
-public abstract class ToAssign {
-  final protected String stack = "stack";
-  final protected String binTree = "BinTree";
-  public abstract String toString();
-
-  public abstract String toPython();
-}
